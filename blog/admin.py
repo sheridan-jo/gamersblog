d31afa3055197cf81a36b7a11f7d668b5b3cd7aa
sheridan-jo <jsolenski@hotmail.com>
2025-07-02 23:58:12 -0400

    Update admin.py
    - Customized list display
    - Made comment list searchable by name and email
    - Created inline admin model for the Comment model and registered it on the Post admin class

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from . import models
+from .models import Comment
+
+#  Inline admin model for the Comment model
+class CommentInline(admin.TabularInline):
+    model = Comment  #  Shows comments related to a post in the Post's admin panel
+    extra = 0  #  No extra form fields shown
+    fields = ('name', 'email', 'text', 'approved')  #  Fields displayed in inline form
+    readonly_fields = ('name', 'email', 'text')  #  Fields that can be seen, but not changed
 
 #  Custom ModelAdmin for the Post model
 class PostAdmin(admin.ModelAdmin):
@@ -24,10 +32,34 @@ class PostAdmin(admin.ModelAdmin):
 
     prepopulated_fields = {'slug': ('title',)}   # Automates generation of slugs
 
+    #  Allows comments to be viewed directly from the Post's admin page
+    #  without going to a separate screen
+    inlines = [CommentInline]
+
 #  Custom ModelAdmin for the Topic model
 class TopicAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']  #  Topic fields to be displayed
 
+#  Custom ModelAdmin for the Comment model
+class CommentAdmin(admin.ModelAdmin):
+    list_display =  ( #  Fields displayed in list view
+        'post',
+        'name',
+        'email',
+        'approved',
+        'created',
+        'updated',
+    )
+
+    search_fields = (  #  Fields that can be searched
+        'name',
+        'email',
+    )
+
+    #  Filters list by comments that are approved and not approved
+    list_filter = ('approved',)
+
 #  Register the models
 admin.site.register(models.Post, PostAdmin)
 admin.site.register(models.Topic, TopicAdmin)
+admin.site.register(models.Comment, CommentAdmin)
